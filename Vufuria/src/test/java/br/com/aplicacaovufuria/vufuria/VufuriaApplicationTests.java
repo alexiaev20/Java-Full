1df21ab9795a48f119dd37376f26ecee4169b5ec
Alexia evelyn <101738715+alexiaev20@users.noreply.github.com>
2024-07-30 14:08:08 -0300

    subindo projeto

---
+++ Vufuria/src/test/java/br/com/aplicacaovufuria/vufuria/VufuriaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.aplicacaovufuria.vufuria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VufuriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
